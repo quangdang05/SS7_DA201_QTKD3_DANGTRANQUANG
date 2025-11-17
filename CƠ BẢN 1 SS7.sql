50dc59b1fa38761924d24c006ebfc7dad690d7ad
quangdang05 <quangdangbee@gmail.com>
2025-11-17 11:03:25 +0700

    Add files via upload

---
+++ CƠ BẢN 1 SS7.sql
@@ -0,0 +1,36 @@
+CREATE DATABASE SAN_PHAM;
+USE SAN_PHAM;
+CREATE TABLE SAN_PHAM(
+id INT PRIMARY KEY,
+ten_san_pham VARCHAR(255),
+danh_muc VARCHAR(100),
+gia_ban INT,
+so_luong_ton INT
+);
+insert into  SAN_PHAM (id,ten_san_pham,danh_muc,gia_ban,so_luong_ton)
+ values 
+(1, 'Laptop Dell XPS 13', 'Laptop', 28000000, 15),
+
+(2, 'iPhone 14 Pro Max', 'Điện thoại', 32000000, 30),
+
+(3, 'Bàn phím cơ Logitech', 'Phụ kiện', 2500000, 50),
+
+(4, 'Laptop MacBook Air M2', 'Laptop', 29500000, 20),
+
+(5, 'Samsung Galaxy S23 Ultra', 'Điện thoại', 26000000, 25),
+
+(6, 'Chuột không dây Fuhlen', 'Phụ kiện', 450000, 100),
+
+(7, 'Tai nghe Sony WH-1000XM5', 'Phụ kiện', 8500000, 40),
+
+(8, 'Laptop HP Envy 15', 'Laptop', 24000000, 18);
+select * from SAN_PHAM WHERE GIA_BAN BETWEEN 20000000 AND 30000000;
+SELECT * FROM SAN_PHAM WHERE  TEN_SAN_PHAM LIKE 'LAPTOP';
+SELECT COUNT(*) AS so_luong_phu_kien
+FROM san_pham
+WHERE danh_muc = 'Phụ kiện';
+SELECT * FROM SAN_PHAM ORDER BY GIA_BAN DESC LIMIT 3;
+
+
+
+
